ba16c9d75a72a9dfd94105569665880f96f6bc22
howardmed6 <howardmed7@gmail.com>
2025-08-09 23:21:34 +0000

    dcgdfhfdjh

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TopBanner from './components/TopBanner';
+import MainHeader from './components/MainHeader';
+import Footer from './components/Footer';
 
-function App() {
+// Importar páginas
+import HomePage from './pages/HomePage';
+import BestsellersPage from './pages/BestsellersPage';
+import ArchivoPage from './pages/ArchivoPage';
+import ColeccionesPage from './pages/ColeccionesPage';
+import NosotrosPage from './pages/NosotrosPage';
+import OfertasPage from './pages/OfertasPage';
+import BlogPage from './pages/BlogPage';
+
+// Importar estilos globales
+import './styles/globals.css';
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'bestsellers': return <BestsellersPage />;
+      case 'archivo': return <ArchivoPage />;
+      case 'colecciones': return <ColeccionesPage />;
+      case 'nosotros': return <NosotrosPage />;
+      case 'ofertas': return <OfertasPage />;
+      case 'blog': return <BlogPage />;
+      default: return <HomePage setCurrentPage={setCurrentPage} />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <TopBanner />
+      <MainHeader currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {renderPage()}
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
